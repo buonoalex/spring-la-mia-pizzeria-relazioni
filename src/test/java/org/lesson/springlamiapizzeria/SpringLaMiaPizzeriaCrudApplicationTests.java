7eff9057111d30e2638ee9e4d68ce455f612d4f5
buonoalex <loopatthecity@gmail.com>
2024-01-09 10:39:10 +0100

    Start project

---
+++ src/test/java/org/lesson/springlamiapizzeria/SpringLaMiaPizzeriaCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package org.lesson.springlamiapizzeria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLaMiaPizzeriaCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
